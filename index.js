8ff88f767f0024c56212a088fe38c919add46532
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-03-12 21:49:11 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+for(let age=30; age<40 ; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(people,event){
+    const messages = [];
+    for (let i = 0; i < people.length; i++) {
+        messages.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+        
+      }
+      return messages;
+}
+
+function countDown(numb){
+    
+    while(numb>=0){
+    console.log(numb--)
+ }
+
+}
